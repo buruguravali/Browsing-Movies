0a19dfb4d6d571db965a2c2709c93a119bdfbb56
ravali <burugu.ravali@gmail.com>
2022-08-25 13:59:17 +0100

    first commit

--- BrowsingMoviesUITests/BrowsingMoviesUITests.swift
+++ BrowsingMoviesUITests/BrowsingMoviesUITests.swift
@@ -17,7 +17,7 @@ class BrowsingMoviesUITests: XCTestCase {
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
-
+//sample change
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
